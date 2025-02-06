67e8ef4d23eaaa2d9219ba29f8dcf566730fc219
E R I C S C H M I D T <info@ericschmidt.de>
2025-02-06 13:49:05 +0100

    tweak history tile

--- src/app/components/history.tsx
+++ src/app/components/history.tsx
@@ -1,5 +1,5 @@
 import { Avatar, Button, Paper } from "@mantine/core";
-import { IconSearch, IconTrash } from "@tabler/icons-react";
+import { IconEyeDotted, IconSearch, IconTrash } from "@tabler/icons-react";
 import Link from "next/link";
 import { JSX, useEffect, useState } from "react";
 import { CompanyInStorage, PersonInStorage } from "../lib/interfaces";
@@ -32,53 +32,57 @@ export default function HistoryList<T>({
   };
 
   return (
-    <Paper p="lg" shadow="lg" className="flex flex-col justify-between gap-4">
-      <h2>Kürzlich besuchte {title}</h2>
-      {history.length > 0 ? (
-        <>
-          <div className="flex flex-col gap-2">
-            {history.map((item, index) => (
-              <Link
-                key={index}
-                href={`${linkPrefix}/${
-                  (item as CompanyInStorage | PersonInStorage).kdnr
-                }`}
-              >
-                <div className="flex items-center gap-4">
-                  <Avatar size={48} radius="md">
-                    {getAvatar(item)}
-                  </Avatar>
-                  <div className="flex flex-col">
-                    <h3>{getTitle(item)}</h3>
-                    <p className="dimmed">{getSubtitle(item)}</p>
+    <Paper p="lg" shadow="lg" className="flex flex-col gap-4">
+      <div className="h-full flex flex-col gap-4 place-content-between">
+        <h2>Kürzlich besuchte {title}</h2>
+        {history.length > 0 ? (
+          <>
+            <div className="flex flex-col gap-2">
+              {history.map((item, index) => (
+                <Link
+                  key={index}
+                  href={`${linkPrefix}/${
+                    (item as CompanyInStorage | PersonInStorage).kdnr
+                  }`}
+                >
+                  <div className="flex items-center gap-4">
+                    <Avatar size={48} radius="md">
+                      {getAvatar(item)}
+                    </Avatar>
+                    <div className="flex flex-col">
+                      <h3>{getTitle(item)}</h3>
+                      <p className="dimmed">{getSubtitle(item)}</p>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            ))}
-          </div>
-          <Button
-            variant="transparent"
-            leftSection={<IconTrash size={16} />}
-            onClick={clearHistory}
-          >
-            Verlauf löschen
-          </Button>
-        </>
-      ) : (
-        <>
-          <p className="dimmed text-center">Keine {title} besucht.</p>
-          <Button
-            color="dark"
-            variant="light"
-            leftSection={<IconSearch size={16} />}
-            fullWidth
-            component={Link}
-            href={`${linkPrefix}`}
-          >
-            {title} finden
-          </Button>
-        </>
-      )}
+                </Link>
+              ))}
+            </div>
+            <Button
+              variant="transparent"
+              leftSection={<IconTrash size={16} />}
+              onClick={clearHistory}
+            >
+              Verlauf löschen
+            </Button>
+          </>
+        ) : (
+          <>
+            <div className="flex flex-col items-center gap-2">
+              <IconEyeDotted size={48} color="gray" />
+              <p className="dimmed text-center">Keine {title} besucht.</p>
+            </div>
+            <Button
+              color="dark"
+              variant="light"
+              leftSection={<IconSearch size={16} />}
+              component={Link}
+              href={`${linkPrefix}`}
+            >
+              {title} finden
+            </Button>
+          </>
+        )}
+      </div>
     </Paper>
   );
 }
