fdc05e31342bc14a224ce059f68a225577a37442
E R I C S C H M I D T <info@ericschmidt.de>
2025-09-29 07:58:38 +0200

    fix dropdown background

--- src/app/campaign/[id]/page.tsx
+++ src/app/campaign/[id]/page.tsx
@@ -136,8 +136,8 @@ export default function Page({ params }: { params: Promise<{ id: string }> }) {
               src={`/brands/${campaign.brand
                 .replaceAll(" ", "-")
                 .toUpperCase()}.png`}
-              width={24}
-              height={24}
+              width={32}
+              height={32}
               alt={`${campaign.brand} Logo`}
               className="inverted object-contain"
             />{" "}
@@ -321,11 +321,11 @@ export default function Page({ params }: { params: Promise<{ id: string }> }) {
                 readOnly={!edit}
               />
               {search && suggestions.length > 0 && (
-                <div className="absolute z-10 w-full mt-1 bg-white border border-gray-300 shadow-lg max-h-60 overflow-y-auto">
+                <div className="absolute z-10 w-full mt-1 bg-[var(--background-subtle)] border border-[var(--subtle)] shadow-lg max-h-60 overflow-y-auto">
                   {suggestions.map((c) => (
                     <div
                       key={c.kdnr}
-                      className="px-3 py-2 cursor-pointer hover:bg-blue-50"
+                      className="px-3 py-2 cursor-pointer hover:bg-[var(--background)]"
                       onClick={() => selectCompany(c)}
                     >
                       <p>
