8b4fd70ce5be6ca73d7962557579c728d635fce8
E R I C S C H M I D T <info@ericschmidt.de>
2025-11-11 13:34:02 +0100

    fix

--- src/app/lib/constants.ts
+++ src/app/lib/constants.ts
@@ -1,7 +1,7 @@
 export const MEINL_WEB_API =
-  process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
-    ? "https://api.meinl.loc/MeinlWebApi/api"
-    : "https://apidev.meinl.de/MeinlWebApi/api";
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
+    ? "https://apidev.meinl.de/MeinlWebApi/api"
+    : "https://api.meinl.loc/MeinlWebApi/api";
 export const MEINL_OFFICE_SIDEBAR_KEY = "meinl.office.sidebar.collapsed";
 export const MEINL_OFFICE_COMPANY_HISTORY_KEY = "meinl.office.company.recent";
 export const MEINL_OFFICE_DEALER_HISTORY_KEY = "meinl.office.dealer.recent";
