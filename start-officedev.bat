6504ed9d12418af7ed258daf56cc6c7f03ae3076
E R I C S C H M I D T <info@ericschmidt.de>
2026-01-07 14:52:38 +0100

    update start scripts

--- start-officedev.bat
+++ start-officedev.bat
@@ -1,3 +1,5 @@
 @echo off
 cd /d C:\inetpub\officedev.meinl.loc
+
+set NODE_OPTIONS=--use-system-ca
 npm run start -- --port 3000
--- start-officeprod.bat
+++ start-officeprod.bat
@@ -1,3 +1,5 @@
 @echo off
 cd /d C:\inetpub\office.meinl.loc
+
+set NODE_OPTIONS=--use-system-ca
 npm run start -- --port 3000
